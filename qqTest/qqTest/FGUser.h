a03046fc44bc0b5b304fc3ae8230d11630b7bd04
lwf <957804349@qq.com>
2016-09-06 18:02:45 +0800

    添加用户模型

---
+++ qqTest/qqTest/FGUser.h
@@ -0,0 +1,13 @@
+//
+//  FGUser.h
+//  qqTest
+//
+//  Created by LWF on 16/9/6.
+//  Copyright © 2016年 优印. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FGUser : NSObject
+
+@end
---
+++ qqTest/qqTest/FGUser.m
@@ -0,0 +1,13 @@
+//
+//  FGUser.m
+//  qqTest
+//
+//  Created by LWF on 16/9/6.
+//  Copyright © 2016年 优印. All rights reserved.
+//
+
+#import "FGUser.h"
+
+@implementation FGUser
+
+@end
