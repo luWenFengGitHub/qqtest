5ef23508c775cdaf10357f97b38f2cd29333a518
lwf <957804349@qq.com>
2016-09-06 17:24:04 +0800

    初始化项目

---
+++ qqTest/qqTest/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  qqTest
+//
+//  Created by LWF on 16/9/6.
+//  Copyright © 2016年 优印. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"qq测试";
+    self.view.backgroundColor = [UIColor yellowColor];
+}
+
+@end
